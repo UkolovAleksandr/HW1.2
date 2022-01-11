df10ec1c23ec7d3d2dd3203d1cf67ce1913063c7
A.Ukolov <sanjrostov@mail.ru>
2022-01-11 23:31:47 +0300

    Initial Commit

---
+++ HW1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1.2
+//
+//  Created by Александр Уколов on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
