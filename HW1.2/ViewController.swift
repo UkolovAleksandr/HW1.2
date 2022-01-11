bbc77b3e08ee80b3dc3f2873c197423071608a46
A.Ukolov <sanjrostov@mail.ru>
2022-01-12 00:43:08 +0300

    iuoi

--- HW1.2/ViewController.swift
+++ HW1.2/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum EEELight {
+    case red, yellow, green }
 
+class ViewController: UIViewController {
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLigt: UIView!
+    @IBOutlet var greenLigt: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var eEELight = EEELight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        redLight.alpha = lightIsOff
+        yellowLigt.alpha = lightIsOff
+        greenLigt.alpha = lightIsOff
+        }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLigt.layer.cornerRadius = redLight.frame.width / 2
+        greenLigt.layer.cornerRadius = redLight.frame.width / 2
     }
-
-
+    
+    @IBAction func startButtonPresed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        switch eEELight {
+            
+        case .red:
+            greenLigt.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            eEELight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLigt.alpha = lightIsOn
+            eEELight = .green
+        case .green:
+            greenLigt.alpha = lightIsOn
+            yellowLigt.alpha = lightIsOff
+            eEELight = .green
+        }
+    }
 }
 
